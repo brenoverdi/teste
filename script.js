1d0d7d10c4e8dfd6fa42dfc138de33a4f5c0ac36
brenoverdi <breno.verdi@acad.pucrs.br>
2019-02-18 10:40:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'https://sagah.blackboard.com/learn/api/public/v1/users/', true);
+
 request.withCredentials = true;
 request.setRequestHeader('Access-Control-Allow-Origin', "*");
 request.setRequestHeader("Content-Type", "application/json");
 request.setRequestHeader("Access-Control-Allow-Headers", "accept, content-type");
-request.setRequestHeader('Authorization', 'Bearer pi510xyyLv7uZ4tB0ogZplwnxYyZpvSz');
+request.setRequestHeader('Authorization', 'Bearer 1toYMIMOGgxs3tEn3w3M2jIuhKgVNWCs');
+request.open('GET', 'https://sagah.blackboard.com/learn/api/public/v1/users/', true);
 request.onload = function() {
 	
 	var data = JSON.parse(this.response);
 	 if (request.status >= 200 && request.status < 400) {
     data.forEach(users => {
-      console.log(userName.title);
+  	const h1 = document.createElement('h1');
+  	h1.textContent = results.userName;
+
+     console.log(results.userName);
     });
   } else {
     console.log('error');
